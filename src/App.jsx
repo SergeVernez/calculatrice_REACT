f6311c22359624c5488599b220f318945be05614
SergeVernez <sergevernez.pro@gmail.com>
2025-01-14 15:54:13 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,9 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>
-          Calculatrice
-        </h1>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + coucou toto</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section>
+        <h1>Coucou</h1>
+      </section>
     </>
   )
 }
