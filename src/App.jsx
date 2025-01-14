5230dd826ca8bf78d0e7d20db9161325cec42368
SergeVernez <sergevernez.pro@gmail.com>
2025-01-14 16:32:56 +0100

    ajout touches

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,51 @@ function App() {
   return (
     <>
       <section>
-        <h1>Coucou</h1>
+        {/* afficher le resultat */}
+        <div className='result'>
+          <h1>Result</h1>
+        </div>
+        {/* pavé */}
+        <div className='keypad'>
+
+          <div className='line'>
+            <button>AC</button>
+            <button>&#9003;</button>
+            <button>+/-</button>
+            <button>&divide;</button>
+          </div>
+
+          <div className='line'>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>x</button>
+          </div>
+
+          <div className='line'>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>-</button>
+          </div>
+
+          <div className='line'>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>+</button>
+          </div>
+
+          <div className='line'>
+            <button>%</button>
+            <button>0</button>
+            <button>,</button>
+            <button>=</button>
+          </div>
+        </div>
+
       </section>
+
     </>
   )
 }
