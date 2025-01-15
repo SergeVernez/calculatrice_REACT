f1b30504956d06b885a9df48fed63d58b344d920
SergeVernez <sergevernez.pro@gmail.com>
2025-01-15 11:16:10 +0100

    fix css

--- src/App.jsx
+++ src/App.jsx
@@ -13,42 +13,45 @@ function App() {
         <div className='result'>
           <h1>Result</h1>
         </div>
+        <div>
+          <hr />
+        </div>
         {/* pavé */}
         <div className='keypad'>
 
           <div className='line'>
-            <button className='yellow'>AC</button>
-            <button className='yellow'>&#9003;</button>
-            <button className='yellow'>+/-</button>
-            <button className='yellow'>&divide;</button>
+            <button className='yellowButton'>AC</button>
+            <button className='yellowButton'>&#9003;</button>
+            <button className='yellowButton'>+/-</button>
+            <button className='yellowButton'>&divide;</button>
           </div>
 
           <div className='line'>
             <button>7</button>
             <button>8</button>
             <button>9</button>
-            <button className='yellow'>&times;</button>
+            <button className='yellowButton'>&times;</button>
           </div>
 
           <div className='line'>
             <button>4</button>
             <button>5</button>
             <button>6</button>
-            <button className='yellow'>-</button>
+            <button className='yellowButton'>-</button>
           </div>
 
           <div className='line'>
             <button>1</button>
             <button>2</button>
             <button>3</button>
-            <button className='yellow'>+</button>
+            <button className='yellowButton'>+</button>
           </div>
 
           <div className='line'>
             <button>%</button>
             <button>0</button>
             <button>,</button>
-            <button className='yellowWhite'>=</button>
+            <button className='yellowWhiteButton'>=</button>
           </div>
         </div>
 
