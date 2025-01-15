44d4386f9b83cead17e09f6cfafbf86b9cbce4ce
SergeVernez <sergevernez.pro@gmail.com>
2025-01-15 16:44:24 +0100

    rempplacement calculable caractères spéciaux

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,27 @@ import './App.css'
 function App() {
   const [total, setTotal] = useState("")
 
-  const handleClick = (valeur) => {
-    setTotal(total.toString() + valeur.toString());
-  }
+  // const handleClick = (valeur) => {
+  //   setTotal(total.toString() + valeur.toString());
+  // }
+
+  const handleClick = (valeur) => { setTotal(total + valeur); }; const handleCalcul = () => { 
+     let caractereCalculable = total.replace(/×/g, '*').replace(/÷/g, '/'); if (caractereCalculable) { try { setTotal(eval(caractereCalculable).toString()); } catch (e) { setTotal('Erreur'); } } };
+  // Conversion des caractères spéciaux en équivalents JavaScript pour le calcul // On remplace toutes les occurrences de '×' par '*' (multiplication) // et '÷' par '/' (division) pour permettre à JavaScript de les interpréter correctement. Les barres obliques (slashes) délimitent l'expression régulière. Tout ce qui se trouve entre ces barres est l'expression régulière à rechercher. Le modificateur g signifie "global". Il indique que la recherche doit être effectuée sur toute la chaîne, et non pas s'arrêter à la première occurrence. Cela permet de remplacer toutes les occurrences du caractère spécifié.
 
 
   // const handleCalcul = () => {
   //   setTotal(eval(total))
   // }
-  const handleCalcul = () => {
-    if (total) {
-      try {
-        setTotal(eval(total));
-      } catch (e) {
-        setTotal("Error");
-      }
-    }
-  };
+  // const handleCalcul = () => {
+  //   if (total) {
+  //     try {
+  //       setTotal(eval(total));
+  //     } catch (e) {
+  //       setTotal("Error");
+  //     }
+  //   }
+  // };
 
   const reset = () => {
     setTotal('');
